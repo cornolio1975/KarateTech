baa09f39861f894926c9629faf87cf5fe1930b11
cornolio1975 <108326822+cornolio1975@users.noreply.github.com>
2026-07-23 15:08:32 +0000

    Deploying to gh-pages from @ cornolio1975/KarateTech@67cc07d0db4da5c34ea796d3431f5b0d9ea1606b 🚀

--- _next/static/CRqYd4ltrOso80JQgRM-s/_buildManifest.js
+++ _next/static/CRqYd4ltrOso80JQgRM-s/_buildManifest.js
@@ -1,7 +1,12 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/KarateTech//_next/:path+",
+        "destination": "/KarateTech/_next/:path+"
+      }
+    ],
     "fallback": []
   },
   "sortedPages": [
